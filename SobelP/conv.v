f284a55dd91585dfc6a06388d51e182a185769c8
milicevicmelani <73944625+milicevicmelani@users.noreply.github.com>
2025-10-08 14:58:15 +0200

    Add files via upload
    Sobel edge detection hardware implementation in Vivado Design Suite

---
+++ SobelP/conv.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.08.2025 18:28:48
+// Design Name: 
+// Module Name: conv
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module conv(
+
+    );
+endmodule
